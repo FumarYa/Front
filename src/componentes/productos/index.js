4d71e8ff20c63baae17d40a58f122c27c111d854
Caastan <juancc2607@gmail.com>
2023-06-06 17:48:50 +0200

    Segunda subida componente Productos y css

--- src/componentes/productos/index.js
+++ src/componentes/productos/index.js
@@ -1,32 +1,36 @@
 import React from "react";
-import Imagen from "../../images/Imagen17.png"
+import Imagen from "../../images/Imagen17.png";
 
-export const ProductosLista = () =>{
-    return(
-        <>{/* Ayuda a que no retorne componentes padre solo a componentes hijo */}
-        <h1 className="title">PRODUCTOS</h1>
-        <div className="productos">
-            <a href="#">
-           <div className="producto">
-                <img src={Imagen} alt=""/>
-           </div>
-           </a>
-           <div className="producto_footer">
-                <h1> Title </h1>
-                <p> Marca </p> 
-                <p className="price">Precio</p> 
-                <p> Tipo </p> 
-           </div>
-           <div className="button">
-                <button className="btn">
-                    Añadir al carrito
-                </button>
-                <div>
-                    <a href="#" className="btn">Vista</a>
-                </div>
-           </div>
-
+export const ProductosLista = () => {
+  return (
+    <>
+      {/* Ayuda a que no retorne componentes padre solo a componentes hijo */}
+      <h1 className="title">PRODUCTOS</h1>
+      <div className="productos">
+        <div className="producto">
+          <a href="#">
+            <div className="producto_img">
+              <img src={Imagen} alt="" />
+            </div>
+          </a>
+          <div className="producto_footer">
+            <h1> Title </h1>
+            <p> Marca </p>
+            <p className="price">Precio</p>
+            <p> Tipo </p>
+          </div>
+          <div className="buttom">
+            <button className="btn">
+                Añadir al carrito
+            </button>
+            <div>
+              <a href="#" className="btn">
+                Vista
+              </a>
+            </div>
+          </div>
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
