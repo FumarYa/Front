e025370b13e873880321a8dd4432dc84f4060bf1
Caastan <juancc2607@gmail.com>
2023-06-07 17:59:19 +0200

    Subida productos

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Header } from './componentes/Header';
-import { ProductosLista } from './componentes/Productos';
-/* import {ApiProducto} from './componentes/routes/index'; */
 import 'boxicons';
 import { BrowserRouter as Router} from "react-router-dom"
 import {Paginas} from "./componentes/Paginas"; 
-import { Dataprovider } from './context/Dataprovider';
+import { Carrito } from './componentes/Carrito';
 
 function App() {
   return (
     <div className="App">
       <Router>
       <Header />
-      
+      <Carrito />  
       <Paginas />
       </Router>
     </div>
